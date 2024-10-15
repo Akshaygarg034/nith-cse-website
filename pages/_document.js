16f83aab5102135ce446831d8bb7b473bb61c903
Akshay Garg <gargakshay034@gmail.com>
2024-10-16 01:13:30 +0530

    made changes for google seo

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>NITH CSE</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Computer Science and Engineering Department of NIT Hamirpur" />
+        <meta name="google-site-verification" content="EcgB5b4stpvGlTDJE3xjdzpN9SLIxgGFcUAsE3PWdTw" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <body>
-          <Main />
-          <NextScript />
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
