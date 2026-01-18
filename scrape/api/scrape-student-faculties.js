31bbd2dbef8b77c86f0b2a605df32439d15f4a49
Akshay Garg <gargakshay034@gmail.com>
2026-01-18 23:28:07 +0530

    creating 2 cron jobs due to free vercel plan

--- scrape/api/scrape-student-faculties.js
+++ scrape/api/scrape-student-faculties.js
@@ -1,10 +1,13 @@
 import scrapeFaculties from '../scraper-functions/scrapeFaculties.js';
+import scrapeStudents from '../scraper-functions/scrapeStudents.js';
 
 export default async function handler(req, res) {
   try {
     console.log('Running scrapeFaculties...');
     await scrapeFaculties();
-    res.status(200).json({ success: true, message: 'scrapeFaculties completed' });
+    console.log('Running scrapeStrudents...');
+    await scrapeStudents()
+    res.status(200).json({ success: true, message: 'Scraping Students and Faculty completed' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: error.message });
